f50d1b842ac14813a89dc5299091aa2ca061c46d
ben Sautner <ben@sautner.me>
2021-02-10 14:20:45 -0500

    basic stop and go before crash

--- Arduino/SecBot/SecBot.ino
+++ Arduino/SecBot/SecBot.ino
@@ -1,4 +1,4 @@
-//this is the working sketch for wandering around 
+//this is the working sketch for wandering around
 
 #include <SoftwareSerial.h>
 #include <SyRenSimplified.h>
@@ -8,55 +8,65 @@ SoftwareSerial SWSerial(NOT_A_PIN, 2); // RX on no pin (unused), TX on pin 11 (t
 SyRenSimplified SR(SWSerial); // Use SWSerial as the serial port.
 
 Servo myservo;
-int pos = 0;
 
 
 //SONAR
 int trigPin = 13;    // Trigger
 int echoPin = 5;    // Echo
+int servoPin = 12;
 long duration, cm, inches;
 unsigned long lastSonarPing;
-unsigned long lastObsticalTimestamp;
-boolean obstructed = false;
+int lastSonarCm = 0;
 
 
 //SERIAL COM
 String inputString = "";         // a String to hold incoming data
 bool stringComplete = false;  // whether the string is complete
 
+bool isInit = false;
+
 void setup()
 {
   SWSerial.begin(9600);
-   Serial.begin(9600);
-     while (!Serial) {
+  Serial.begin(9600);
+  while (!Serial) {
     ; // wait for serial port to connect. Needed for native USB port only
   }
 
-   myservo.attach(9);
-   
+  
 
-//    //Sonar Setup
-   pinMode(trigPin, OUTPUT);
-   pinMode(echoPin, INPUT);
 
-inputString.reserve(200);
-   
+  //    //Sonar Setup
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+
+  inputString.reserve(200);
+
 }
 
 void loop()
 {
+  if (! isInit) {
+      SR.motor(0);
+      steerServo(45);
+      delay(2000);
+      isInit = true;
+
+  }
   int power;
 
- if (stringComplete) {
-    
+  if (stringComplete) {
+
     // clear the string:
     inputString = "";
     stringComplete = false;
   }
- 
-//SONAR LOOP
 
-   if (millis() - lastSonarPing > 100) {
+  //SONAR LOOP
+
+  if (millis() - lastSonarPing > 100) {
+  
+      
     digitalWrite(trigPin, LOW);
     delayMicroseconds(5);
     digitalWrite(trigPin, HIGH);
@@ -65,153 +75,93 @@ void loop()
     pinMode(echoPin, INPUT);
     duration = pulseIn(echoPin, HIGH);
     // Convert the time into a distance
-    cm = (duration/2) / 29.1;     // Divide by 29.1 or multiply by 0.0343
- 
- 
-    lastSonarPing = millis();
-
-    if (! obstructed && cm < 5) {
-        SR.motor(0);
-        Serial.print("front_sonar,");
-        Serial.println(cm);
-   
-        lastObsticalTimestamp = millis();
-        obstructed = true;
-    } else {
-       if (obstructed && millis() - lastObsticalTimestamp > 250) {
-// 
-//          Serial.print("front_sonar,");
-//          Serial.println(cm);
-           obstructed = false;
-           //SR.motor(10);
-       }
-    }
-    
+    cm = (duration / 2) / 29.1;   // Divide by 29.1 or multiply by 0.0343
   
 
-  }
+    lastSonarPing = millis();
+    if (cm > lastSonarCm +1 || cm < lastSonarCm - 1) {
+           Serial.print("front_sonar:");
+           Serial.println(cm);
+           lastSonarCm = cm;
+           if (cm < 5) {
+              SR.motor(0);
+           }
 
- 
-//   for (pos = 0; pos <= 80; pos += 1) { // goes from 0 degrees to 180 degrees
-//    // in steps of 1 degree
-//    myservo.write(pos);              // tell servo to go to position in variable 'pos'
-//    delay(15);                       // waits 15ms for the servo to reach the position
-//   
-//  }
-// 
-//  for (pos = 80; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
-//    myservo.write(pos);              // tell servo to go to position in variable 'pos'
-//    delay(15);                       // waits 15ms for the servo to reach the position
-//     Serial.println(pos);
-//  }
+    }
+  
+
+
+
+  }
 }
 
-  void serialEvent() {
+void serialEvent() {
   while (Serial.available()) {
     // get the new byte:
     char inChar = (char)Serial.read();
-    // add it to the inputString:
     inputString += inChar;
-    // if the incoming character is a newline, set a flag so the main loop can
-    // do something about it:
     if (inChar == '\n') {
-        processCommand(inputString);
+      processCommand(inputString);
       stringComplete = true;
-    
+
     }
-    
+
   }
- }
-
- void processCommand(String command) {
-
-    Serial.print(inputString);
-    String cmd = getValue(inputString, ':', 0);
-    String val = getValue(inputString, ':', 1);
-      Serial.print("command: " + cmd);
-      Serial.println("value: " + val);
-
-        if (cmd == "M1") {
-            SR.motor(val.toInt());
-        } 
-        else 
-        if (cmd == "S1") {
-            steerServo(val.toInt());
-        }
-  
- }
-
- void steerServo(int newPos) {
-
-  Serial.print("Steering ");
-    Serial.print(pos);
-      Serial.print(" to ");
-        Serial.println(newPos);
-
-  if (pos >= newPos) {
-
-  for (pos = newPos; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
-      Serial.print("Turning Right " ) ;
-       Serial.println(pos);
-    myservo.write(pos);              // tell servo to go to position in variable 'pos'
-    delay(15);                       // waits 15ms for the servo to reach the position
-  }
-    
-  } else {
-
-  for (pos; pos <= newPos; pos += 1) { // goes from 0 degrees to 180 degrees
-    // in steps of 1 degree
-       Serial.print("Turning Left " ) ;
-       Serial.println(pos);
-    myservo.write(pos);              // tell servo to go to position in variable 'pos'
-    delay(15);                       // waits 15ms for the servo to reach the position
-  }
-    
-  }
-
-
-
-  
- }
-
- String getValue(String data, char separator, int index)
-{
-    int found = 0;
-    int strIndex[] = { 0, -1 };
-    int maxIndex = data.length() - 1;
-
-    for (int i = 0; i <= maxIndex && found <= index; i++) {
-        if (data.charAt(i) == separator || i == maxIndex) {
-            found++;
-            strIndex[0] = strIndex[1] + 1;
-            strIndex[1] = (i == maxIndex) ? i+1 : i;
-        }
-    }
-    return found > index ? data.substring(strIndex[0], strIndex[1]) : "";
 }
 
+void processCommand(String command) {
+
+   
+  String cmd = getValue(inputString, ':', 0);
+  String val = getValue(inputString, ':', 1);
+
+  if (cmd == "M1") {
+    powerMotor(val.toInt());
+  }
+  else if (cmd == "S1") {
+    steerServo(val.toInt());
+  }
+
+}
+
+void steerServo(int newPos) {
+
+  myservo.attach(servoPin);
+  delay(100);
+  myservo.write(newPos);
+  delay(100);
+  myservo.detach();
   
-  // Ramp from -127 to 127 (full reverse to full forward), waiting 20 ms (1/50th of a second) per value.
-//  for (pos = 0; pos <= 80; pos += 1) { // goes from 0 degrees to 180 degrees
-//    // in steps of 1 degree
-//    myservo.write(pos);              // tell servo to go to position in variable 'pos'
-//    delay(15);                       // waits 15ms for the servo to reach the position
-//  }
-////  for (power = -10; power <= 10; power ++)
-////  {
-////    SR.motor(power);
-////   delay(20);
-////  }
-////
-//  for (pos = 80; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
-//    myservo.write(pos);              // tell servo to go to position in variable 'pos'
-//    delay(15);                       // waits 15ms for the servo to reach the position
-//  }
-  
-//  // Now go back the way we came.
-//  for (power = 10; power >= -10; power --)
-//  {
-//    SR.motor(power);
-//    delay(20);
-//  }
-//}
+
+
+}
+
+void powerMotor(int val) {
+
+  if (val < 0 && lastSonarCm < 5) {
+    return;
+  }
+  else {
+     SR.motor(val);
+  }
+}
+
+
+String getValue(String data, char separator, int index)
+{
+  int found = 0;
+  int strIndex[] = { 0, -1 };
+  int maxIndex = data.length() - 1;
+
+  for (int i = 0; i <= maxIndex && found <= index; i++) {
+    if (data.charAt(i) == separator || i == maxIndex) {
+      found++;
+      strIndex[0] = strIndex[1] + 1;
+      strIndex[1] = (i == maxIndex) ? i + 1 : i;
+    }
+  }
+  return found > index ? data.substring(strIndex[0], strIndex[1]) : "";
+}
+
+
+ 
--- RaspPi/secbot-pi/src/main/java/com/secbot/Application.kt
+++ RaspPi/secbot-pi/src/main/java/com/secbot/Application.kt
@@ -16,18 +16,32 @@ import java.util.*
 class Application : SerialPortListener {
 
 
+    enum class State {
+        IDLE, STOPPED, BACKINGUP, CLEAR, LOOKING
+    }
 
     private val arduinoSerialInterface = ArduinoSerialInterface(this)
     private lateinit var piCamera: RPiCamera
     private val apiClient = DefaultApiClient()
 
+    private var forwardSpaceCm = -1
+    private var state = State.IDLE
+
     override fun onRead(rawData: String) {
-        println("read: $rawData")
 
 
         if (rawData.isNotEmpty()) {
             val sanitized = rawData.trimEnd('\n').trimEnd('\r').trimEnd('\n')
-            when (sanitized) {
+           println("Serial Data: $sanitized")
+            val data = sanitized.split(":")
+            when (data[0]) {
+
+                "front_sonar" -> {
+                    forwardSpaceCm = data[1].toInt()
+
+                }
+
+
                 "Obstical Detected" -> {
 
                     GlobalScope.async {
@@ -68,17 +82,88 @@ class Application : SerialPortListener {
             .setTimeout(1000)
 
         piCamera.setFullPreviewOn()
+
+
+//        arduinoSerialInterface.sendCommand("S1:45")
+//        arduinoSerialInterface.sendCommand("M1:0")
+
+
     }
 
     @Throws(IOException::class, InterruptedException::class)
     private fun start() {
 
+        val speed = 25
+
         GlobalScope.launch {
-            apiClient.test()
+           // apiClient.test()
+            while (true) {
+//                arduinoSerialInterface.sendCommand("S1:45")
+//                break;
+                println(state)
+                if (forwardSpaceCm > 10 && state != State.LOOKING && state != State.BACKINGUP) {
+                    state = State.CLEAR
+                }
+
+                if (state == State.CLEAR && forwardSpaceCm > 5) {
+                    arduinoSerialInterface.sendCommand("M1:-$speed")
+                }
+                else {
+                    if (forwardSpaceCm < 5) {
+                        arduinoSerialInterface.sendCommand("M1:0")
+                        state = State.STOPPED
+
+                    }
+
+                    if (state == State.STOPPED) {
+                        while (forwardSpaceCm in 0..9) {
+                            println("backing up")
+                            state = State.BACKINGUP
+                            arduinoSerialInterface.sendCommand("M1:$speed")
+                        }
+
+
+
+                    }
+                    if (forwardSpaceCm >= 10 && state == State.BACKINGUP) {
+                        println("looking around")
+                        arduinoSerialInterface.sendCommand("M1:0")
+                        state = State.LOOKING
+                    }
+                    if (state == State.LOOKING) {
+                        println("looking for clear space ")
+                        arduinoSerialInterface.sendCommand("S1:10")
+                        Thread.sleep(1000)
+
+                        arduinoSerialInterface.sendCommand("M1:$speed")
+                        Thread.sleep(2000)
+                        arduinoSerialInterface.sendCommand("M1:0")
+                        Thread.sleep(1000)
+                        arduinoSerialInterface.sendCommand("S1:70")
+                        Thread.sleep(1000)
+                        arduinoSerialInterface.sendCommand("M1:-$speed")
+                        Thread.sleep(1000)
+                        arduinoSerialInterface.sendCommand("M1:0")
+                        Thread.sleep(1000)
+                        arduinoSerialInterface.sendCommand("S1:45")
+                        Thread.sleep(1000)
+                        if (forwardSpaceCm > 10) {
+                            state = State.CLEAR
+                        }
+                    }
+                }
+                Thread.sleep(1000)
+            }
+
+
         }
 
 
         //arduinoSerialInterface!!.sendCommand(Command(CommandType.CALIBRATE.id, "", 0))
+
+       // arduinoSerialInterface.sendCommand("M1:-10")
+
+
     }
 
 
